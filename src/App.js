3294ba0b0a2a948ba8a8c5d9cfb8b44622e33b7b
Desooe-pro <sacha.allardin06@gmail.com>
2025-01-31 16:36:16 +0100

    Tout fini

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import './App.css';
-import MyButton from './MyButton.js'
-import MyCounterButton from "./MyCounterButton";
+import {useState} from 'react';
+// import MyButton from './MyButton.js';
+// import MyCounterButton from "./MyCounterButton";
 
 function Form() {
+    let [todos, setTodos] = useState([]);
+    const [text, setText] = useState('');
+
+    function HandleClick() {
+        setTodos([...todos, text]);
+        setText('');
+        document.getElementById("search-bar").value = ""
+    }
+
+    function HandleSup(items) {
+        const NewTodos = todos.filter(item => todos.indexOf(item) !== todos.indexOf(items))
+        setTodos(todos = NewTodos)
+    }
+
     return (
         <div>
-            <form action="#" method="post" className="box">
-                <input id="search-bar" type="text" placeholder="Tapez votre texte"/>
-                <button>Ajouter</button>
-            </form>
+            <input id="search-bar" type="text" placeholder="Tapez votre texte" onChange={e => setText(e.target.value)}/>
+            <button id="Reset" onClick={HandleClick}>Ajouter</button>
+            <ul style={{listStyle:"none"}}>
+                {todos.map((item, id) => (
+                    <li key={id}>
+                        <div style={{display:"flex", flexDirection:"column", flexWrap:"nowrap", alignItems:"center"}}>
+                            <div style={{display:"flex", flexDirection:"row", flexWrap:"nowrap", justifyContent:"space-around", width:"40%"}}>
+                                <div style={{width:"85%", textAlign:"start", border:"1px solid black", paddingLeft:"10px", paddingBottom:"6px", paddingTop:"3px", background:"#404040", borderRadius:"10px"}}>
+                                    {item}
+                                </div>
+                                <div style={{paddingRight:"1px", paddingTop:"4px"}}>
+                                    <button className="button" onClick={e => HandleSup(item)}>Supprimer</button>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
@@ -24,8 +53,6 @@ function Header() {
 function Main() {
     return (
         <main style={{textAlign: "center"}}>
-            <MyButton/>
-            <MyCounterButton/>
             <Form/>
         </main>
     );
